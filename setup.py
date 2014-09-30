6261032258d30661bc3a81fc0c82bb867793282f
Gautam <gautam.sisodia@gmail.com>
2014-09-30 16:15:42 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = "gentrySeattle",
+    version = "0.1",
+    packages = ["nbdtools"],
+    author = "Yao Gbanaglo and Gautam Sisodia",
+    author_email = "gautam.sisodia@gmail.com",
+    description = "A package of tools to study gentrification manifest in Seattle gov's open data.",
+    url = "https://github.com/gautsi/gentrySeattle"
+)
