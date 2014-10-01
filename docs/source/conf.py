fd38817f0d52edca23767052148044f4f395e77a
Gautam <gautam.sisodia@gmail.com>
2014-09-30 23:10:49 -0700

    mocked matplotlib.colors inn conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,7 @@ class Mock(object):
         else:
             return Mock()
  
-MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot']
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'matplotlib.colors']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
 
