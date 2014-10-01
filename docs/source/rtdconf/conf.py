e6640300d0288be0190f4f610bef70cd3872d293
Gautam <gautam.sisodia@gmail.com>
2014-09-30 22:48:22 -0700

    removed scipy, mock from requirements, added mock class to rtd conf.py

--- docs/source/rtdconf/conf.py
+++ docs/source/rtdconf/conf.py
@@ -17,7 +17,21 @@ import os
 
 
 #To get scipy to load for readthedocs:
-import mock
+class Mock(object):
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return Mock()
+
+    @classmethod
+    def __getattr__(cls, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            return type(name, (), {})
+        else:
+            return Mock()
  
 MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate']
 for mod_name in MOCK_MODULES:
